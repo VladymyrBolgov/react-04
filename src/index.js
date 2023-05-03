2eb33443c90c52fca1babcadba4aa01e9e4bcfaa
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-05-04 01:08:29 +0300

    reader, srs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+// import { Example1 } from 'components/Example1';
+import { Reader } from 'components/Reader/Reader';
 import './index.css';
+import publications from './publications.json';
 
-import reportWebVitals from './reportWebVitals';
+const theme = {};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Reader items={publications} />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <GlobalStyle />
+//       <Reader items={publications} />
+//     </ThemeProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
