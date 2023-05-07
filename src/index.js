9f5669bd627e400c11b721c123a441f0dd8ce86c
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-05-07 20:40:10 +0300

    ind

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
-// import { Example1 } from 'components/Example1';
+import { Example1 } from 'components/Example1';
 import { Reader } from 'components/Reader/Reader';
 import './index.css';
 import publications from './publications.json';
+
 
 const theme = {};
 
@@ -14,16 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Reader items={publications} />
+      <Example1 items={publications} />
     </ThemeProvider>
   </React.StrictMode>,
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <Reader items={publications} />
-//     </ThemeProvider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
